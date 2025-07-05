b4b281c4bd5f73b6185af21f088703e4b282fe5c
jamsamjam <andsunlit@gmail.com>
2025-07-05 14:22:07 +0200

    update config to load css

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  reactStrictMode: false,
+  assetPrefix: isProduction ? `/blog.samlee.ch` : '',
+  basePath: isProduction ? `/blog.samlee.ch` : '',
+  trailingSlash: true,
   images: {
     domains: ['blog.samlee.ch'],
   },
